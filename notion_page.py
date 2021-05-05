7903df39d7bb4e747692c551efc03e1f41598d2b
Kaede <kidhaibara@gmail.com>
2021-05-05 21:44:50 +0800

    Update notion page impl

--- notion_page.py
+++ notion_page.py
@@ -1,22 +1,138 @@
 import os
 
-from notion.block import CodeBlock, ImageBlock
+from notion.block import CodeBlock, ImageBlock, TextBlock, DividerBlock
 
 from config import Config
 
 
 class PageBaseBlock:
     def __init__(self):
-        self.type = 'base'
+        self.id = 'unknown'
+        self.type = 'unknown'
 
     def write_block(self):
-        return self.type
+        return """<!-- unsupported page block
+type: {}
+{}
+-->""".format(self.type, self._wip_msg())
+
+    def _wip_msg(self):
+        return "notion id: " + self.id
 
 
 class PageTextBlock(PageBaseBlock):
     def __init__(self):
         super().__init__()
-        self.text = 'text'
+        self.type = 'text'
+        self.text = ''
+
+    def write_block(self):
+        return self.text
+
+
+class PageEnterBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'enter'
+        self.text = '\n'
+
+    def write_block(self):
+        return self.text
+
+
+class PageDividerBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'divider'
+        self.text = '---'
+
+    def write_block(self):
+        return self.text
+
+
+class PageNumberedListBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'numbered_list'
+        self.text = ''
+
+    def write_block(self):
+        return '0. {}'.format(self.text)
+
+
+class PageBulletedListBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'bulleted_list'
+        self.text = ''
+
+    def write_block(self):
+        return ' - {}'.format(self.text)
+
+
+class PageQuoteBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'quote'
+        self.text = ''
+
+    def write_block(self):
+        return '> {}'.format(self.text)
+
+
+class PageCalloutBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'callout'
+        self.text = ''
+
+    def write_block(self):
+        lines = str(self.text).split("\n")
+        return '> ' + "> ".join(lines)
+
+
+class PageHeaderBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'header'
+        self.text = ''
+
+    def write_block(self):
+        return '# {}'.format(self.text)
+
+
+class PageSubHeaderBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'sub_header'
+        self.text = ''
+
+    def write_block(self):
+        return '## {}'.format(self.text)
+
+
+class PageSubSubHeaderBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'sub_sub_header'
+        self.text = ''
+
+    def write_block(self):
+        return '### {}'.format(self.text)
+
+
+class PageCodeBlock(PageTextBlock):
+    def __init__(self):
+        super().__init__()
+        self.type = 'code'
+        self.text = ''
+        self.lang = ''
+
+    def write_block(self):
+        return """```{}
+{}
+```
+""".format(self.lang, self.text)
 
 
 class PageImageBlock(PageBaseBlock):
@@ -26,6 +142,9 @@ class PageImageBlock(PageBaseBlock):
         self.image_caption = ''
         self.image_url = ''
         self.image_file = ''
+
+    def write_block(self):
+        return "![{}]({})".format(self.image_caption, self.image_url)
 
 
 # noinspection PyBroadException
@@ -43,6 +162,20 @@ class NotionPage:
         self.title = ''
         self.blocks = []
         self.properties = {}
+
+        self.mapping = {
+            "text": self._parse_text,
+            "image": self._parse_image,
+            "divider": self._parse_divider,
+            "numbered_list": self._parse_numbered_list,
+            "bulleted_list": self._parse_bulleted_list,
+            "quote": self._parse_quote,
+            "callout": self._parse_callout,
+            "header": self._parse_header,
+            "sub_header": self._parse_sub_header,
+            "sub_sub_header": self._parse_sub_sub_header,
+            "code": self._parse_code,
+        }
 
     def is_markdown_able(self):
         return self.get_title() is not None  # and self.get_date() is not None
@@ -141,23 +274,51 @@ class NotionPage:
 
         # parse page blocks
         for block in page.children:
-            if type(block) is CodeBlock:
-                if '[properties]' in block.title:
-                    self.parse_properties(block)
-                    continue
-            if type(block) is ImageBlock:
-                self.parse_image(block)
+            if block.type in self.mapping:
+                self.mapping[block.type].__call__(block)
                 continue
-            self.parse_basic(block)
+            # Basic Block Parsing
+            self._parse_basic(block)
         pass
 
-    def parse_basic(self, block):
-        block_type = block.type
+    def _parse_basic(self, block):
         page_block = PageBaseBlock()
-        page_block.type = block_type
+        page_block.id = block.id
+        page_block.type = block.type
         self.blocks.append(page_block)
 
-    def parse_properties(self, block):
+    def _parse_text(self, block):
+        if len(str(block.title).strip()) == 0:
+            self.blocks.append(PageEnterBlock())
+            return
+        page_block = PageTextBlock()
+        page_block.type = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+        pass
+
+    def _parse_image(self, block):
+        image_id = block.id
+        image_caption = str(block.caption)
+        image_url = str(block.source)
+        temp_file = os.path.join(Config.output(), "assets/image", image_id + "_" + image_caption, ".jpg")
+
+        page_block = PageImageBlock()
+        page_block.type = block.id
+        page_block.type = block.type
+        page_block.image_caption = image_caption
+        page_block.image_url = image_url
+        page_block.image_file = temp_file
+        self.blocks.append(page_block)
+
+    def _parse_divider(self, block):
+        page_block = PageDividerBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        self.blocks.append(page_block)
+
+    def _parse_properties(self, block):
         content = block.title
         symbol = '[properties]'
         if symbol in content:
@@ -170,14 +331,73 @@ class NotionPage:
                     value = line[idx + len('='):].strip()
                     self.properties[key] = value
 
-    def parse_image(self, block):
-        image_id = block.id
-        image_caption = str(block.caption)
-        image_url = str(block.source)
-        temp_file = os.path.join(Config.output(), "assets/image", image_id + "_" + image_caption, ".jpg")
+    def _parse_code(self, block: CodeBlock):
+        content = block.title
+        symbol = '[properties]'
+        if symbol in content:
+            self._parse_properties(block)
+            return
 
-        page_image_block = PageImageBlock()
-        page_image_block.image_caption = image_caption
-        page_image_block.image_url = image_url
-        page_image_block.image_file = temp_file
-        self.blocks.append(page_image_block)
+        page_block = PageCodeBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        page_block.lang = block.language
+        self.blocks.append(page_block)
+
+    def _parse_numbered_list(self, block):
+        page_block = PageNumberedListBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_bulleted_list(self, block):
+        page_block = PageBulletedListBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_quote(self, block):
+        page_block = PageQuoteBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_callout(self, block):
+        page_block = PageCalloutBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_header(self, block):
+        page_block = PageHeaderBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_sub_header(self, block):
+        page_block = PageSubHeaderBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_sub_sub_header(self, block):
+        page_block = PageSubSubHeaderBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+
+    def _parse_stub(self, block):
+        page_block = PageBaseBlock()
+        page_block.id = block.id
+        page_block.type = block.type
+        page_block.text = block.title
+        self.blocks.append(page_block)
+        raise Exception('Stub!')

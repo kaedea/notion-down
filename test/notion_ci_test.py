43bbb4df16f7761b5b4195d4b8ed8c9fea65cda6
Kaede <kidhaibara@gmail.com>
2021-05-12 01:11:36 +0800

    Add ci test

---
+++ test/notion_ci_test.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+
+from config import Config
+from notion_reader import NotionReader
+from notion_writer import NotionWriter
+from utils.utils import Utils
+
+
+class NotionCiTest(unittest.TestCase):
+
+    def test_generate_markdown_showcase(self):
+        Config.load_env()
+        Config.set_debuggable(True)
+        Config.set_blog_url("https://www.notion.so/kaedea/Noton-Down-Sample-440de7dca89840b6b3bab13d2aa92a34")
+        Config.set_output(os.path.join(Utils.get_workspace(), "build"))
+
+        NotionWriter.clean_output()
+
+        main_page = NotionReader.read_main_page()
+        self.assertIsNotNone(main_page)
+
+        test_page = Utils.find_one(main_page.children, lambda it: it and str(it.title) == "MarkDown Test Page")
+        self.assertIsNotNone(test_page)
+
+        md_page = NotionReader.handle_single_page(test_page)
+        self.assertIsNotNone(md_page)
+
+        NotionWriter.handle_page(md_page)
+        pass
+
+    def test_generate_read_me(self):
+        Config.load_env()
+        Config.set_debuggable(True)
+        Config.set_blog_url("https://www.notion.so/kaedea/Noton-Down-Sample-440de7dca89840b6b3bab13d2aa92a34")
+        Config.set_output(os.path.join(Utils.get_workspace(), "build"))
+
+        NotionWriter.clean_output()
+
+        main_page = NotionReader.read_main_page()
+        self.assertIsNotNone(main_page)
+
+        test_page = Utils.find_one(main_page.children, lambda it: it and str(it.title) == "NotionDown README")
+        self.assertIsNotNone(test_page)
+
+        md_page = NotionReader.handle_single_page(test_page)
+        self.assertIsNotNone(md_page)
+
+        NotionWriter.handle_page(md_page)
+        pass
+

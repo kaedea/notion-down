2cae52595df50d22f87befd481b13d42a3418d02
Kaede <kidhaibara@gmail.com>
2021-05-26 22:08:54 +0800

    Append commit

--- test/corrects_test.py
+++ test/corrects_test.py
@@ -1,15 +1,10 @@
-import os
-import re
 import unittest
 
-from config import Config
-from corrects.inspect_spell import SpellInspector
-from notion_reader import NotionReader
-from utils.utils import Utils
+from corrects.inspect_spell import PyCorrectorInspector
 
 
 class CorrectsApiTest(unittest.TestCase):
 
-    def test_spelling_inspect(self):
+    def test_pycorrector_spelling_inspect(self):
         text = '少先队员因该为老人让坐'
-        SpellInspector().inspect_text(text)
+        PyCorrectorInspector().inspect_text(text)

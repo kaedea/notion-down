d30cc42134e659d230f20fc9d75ab69c24926f6a
Kaede <kidhaibara@gmail.com>
2021-05-30 21:19:04 +0800

    Update build readme config

--- test/notion_ci_test.py
+++ test/notion_ci_test.py
@@ -34,13 +34,14 @@ class NotionCiTest(unittest.TestCase):
         Config.set_debuggable(True)
         Config.set_blog_url("https://www.notion.so/kaedea/Noton-Down-Sample-440de7dca89840b6b3bab13d2aa92a34")
         Config.set_output(os.path.join(Utils.get_workspace(), "build"))
+        Config.set_page_titles(["NotionDown README"])
 
         NotionWriter.clean_output()
 
         main_page = NotionReader.read_main_page()
         self.assertIsNotNone(main_page)
 
-        test_page = Utils.find_one(main_page.children, lambda it: it and str(it.title) == "NotionDown README")
+        test_page = Utils.find_one(main_page.children, lambda it: it.type == 'page' and str(it.title) in Config.page_titles())
         self.assertIsNotNone(test_page)
 
         md_page = NotionReader.handle_single_page(test_page)

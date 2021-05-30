c0e16100d8cb687019c601e273153fe8c2c976a0
Kaede <kidhaibara@gmail.com>
2021-05-30 19:47:27 +0800

    Extract argsparser to config

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import typing
 
+from utils.utils import Utils
+
 SYS_ENV_MAP = dict(
     token_v2="NOTION_TOKEN_V2",
 )
@@ -87,3 +89,38 @@ class Config:
     @staticmethod
     def to_string():
         return json.dumps(KV, indent=2)
+
+
+class ArgsParser:
+
+    @staticmethod
+    def parse():
+        import argparse
+        parser = argparse.ArgumentParser(description='Process some args.')
+        parser.add_argument('debug', nargs='?', default=True)
+        parser.add_argument('workspace', nargs='?', default=Utils.get_workspace())
+        parser.add_argument('output', nargs='?', default=os.path.join(Utils.get_temp_dir(), "notion-down/outputs"))
+        parser.add_argument('notion_token_v2', nargs='?', default=None)
+        parser.add_argument('blog_url', nargs='?', default=None)
+        parser.add_argument('channels', nargs='?', default='default')
+
+        args = parser.parse_args()
+        if args.debug:
+            args.output = os.path.join(args.workspace, "build/outputs")
+            args.notion_token_v2 = os.environ['NOTION_TOKEN_V2']
+            args.blog_url = "https://www.notion.so/kaedea/Noton-Down-Sample-440de7dca89840b6b3bab13d2aa92a34"
+            pass
+
+        required_args = ['blog_url']
+        for item in required_args:
+            if item not in args or getattr(args, item) is None:
+                raise Exception('{} is null or not presented'.format(item))
+
+        Config.load_env()
+        Config.set_debuggable(args.debug)
+        Config.set_workspace(args.workspace)
+        Config.set_output(args.output)
+        Config.set_token_v2(args.notion_token_v2)
+        Config.set_blog_url(args.blog_url)
+        Config.set_channels(str(args.channels).split("|"))
+        pass

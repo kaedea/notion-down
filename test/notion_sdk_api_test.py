932e5210a252e94412b429702aed4d21b080abfb
Kaede <kidhaibara@gmail.com>
2021-08-18 21:23:25 +0800

    Update notion sdk tests

--- test/notion_sdk_api_test.py
+++ test/notion_sdk_api_test.py
@@ -39,12 +39,53 @@ class NotionCiTest(unittest.TestCase):
         pass
 
     def test_list_page(self):
+        """
+        Page {
+            id,
+            url,
+            properties: [str : Block]
+        }
+        Block {
+            type,
+            (type): str/list/dict
+        }
+        """
         rsp = self.notion.search(filter={"property": "object", "value": "page"})
         pprint(rsp)
         self.assertEqual('list', rsp['object'])
         pages = rsp['results']
         self.assertTrue(len(pages) >= 0)
 
-        blocks = self.notion.blocks.children.list(pages[0]['id'])
-        self.assertIsNotNone(blocks)
+        for page in pages:
+            self.assertEqual('page', page['object'])
+            page_id = page['id']
+            page_url = page['url']
+            page_properties = page['properties']
+            if 'title' in page_properties:
+                title_property = page_properties['title']
+                self.assertEqual('title', title_property['type'])
+                titles = title_property['title']
+                self.assertTrue(len(titles) >= 0)
+        pass
+
+    def test_list_page_blocks(self):
+        rsp = self.notion.search(filter={"property": "object", "value": "page"})
+        pprint(rsp)
+        self.assertEqual('list', rsp['object'])
+        pages = rsp['results']
+        self.assertTrue(len(pages) >= 0)
+
+        page = Utils.find_one(pages, lambda it: it['id'] == '440de7dc-a898-40b6-b3ba-b13d2aa92a34')
+
+        self.assertEqual('page', page['object'])
+        page_id = page['id']
+        rsp = self.notion.blocks.children.list(page_id)
+        self.assertIsNotNone(rsp)
+        self.assertEqual('list', rsp['object'])
+        blocks = rsp['results']
+        for block in blocks:
+            self.assertEqual('block', block['object'])
+            block_id = block['id']
+            block_type = block['type']
+            block_content = block[(block_type)]
         pass
